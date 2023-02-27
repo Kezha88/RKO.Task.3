28af8cd1fe1f5b511a766947ce959e0f3ffe22be
Kezha88 <kuku2304@gmail.com>
2023-02-28 00:58:07 +0300

    Добавьте файлы проекта.

---
+++ RKO.Task.3/View/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using RKO.Task._3.View.Pages;
+using RKO.Task._3.Core;
+using System.ComponentModel;
+using System.Windows;
+
+namespace RKO.Task._3
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            CoreNavigate.MyConnection = MainFrame;
+            MainFrame.Navigate(new MainPage());
+
+        }
+
+    }
+}
